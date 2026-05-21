74bafb344b3d2e41331192ad75e82bd4b6d68ec1
Sabbeer60 <23201160@uap-bd.edu>
2026-05-21 11:44:23 +0600

    Update urls.py

--- gaanhub/gaanhub/urls.py
+++ gaanhub/gaanhub/urls.py
@@ -346,3 +346,329 @@ if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# #from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from music import views as music_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', music_views.landing, name='landing'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+#     path('music/', include('music.urls')),
+#     path('users/', include('users.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
